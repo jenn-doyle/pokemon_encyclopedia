a5c2257faf4984009cede061597399f80a77744c
jenn-doyle <jenndoyle@hotmail.co.uk>
2022-06-14 20:00:49 +0100

    add basic encyclopedia

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+import random
+
+pokemon_id = random.choice(range(1, 152))
+response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+
+pokemon_data = response.json()
+
+# print(pokemon_data['id'])
+# print(pokemon_data['name'])
+
+print(f"You got Pokemon number {pokemon_data['id']}, their name is {pokemon_data['name'].capitalize()}!")
+print(f"Height: {pokemon_data['height']} • Weight: {pokemon_data['weight']}")
+# print(f"Height: {pokemon_data['height']} • Weight: {pokemon_data['weight']}")
+
+pokemon_types = []
+
+for type_data in pokemon_data['types']:
+    pokemon_types.append(type_data['type']['name'])
+
+print(f"Type(s): {' and '.join(pokemon_types)}")
+
+pokemon_abilities = []
+
+for ability_data in pokemon_data['abilities']:
+    pokemon_abilities.append(ability_data['ability']['name'])
+
+print(f"Abilities: {', '.join(pokemon_abilities)}")
